788640c22c969531b5933ea9da4ba294bc002db7
Eli Pousson <eli.pousson@gmail.com>
2024-10-20 17:23:58 -0400

    Add wb_add_cip_pivot_table function

---
+++ R/wb_add_cip_pivot_table.R
@@ -0,0 +1,35 @@
+#' Add a pivot table using data from the Six-Year CIP Sheet
+#'
+wb_add_cip_pivot_table <- function(wb,
+                                   sheet = current_sheet(),
+                                   dims = "A1",
+                                   rows = c("Cost Center Name", "Project Name", "Revenue Category Name"),
+                                   data = curr_yr_span(),
+                                   params = list(
+                                     table_style = "TableStyleLight16",
+                                     apply_number_formats = TRUE,
+                                     first_column = TRUE,
+                                     with_filter = TRUE,
+                                     banded_rows = TRUE # ,
+                                     # numfmt = c(formatCode = rep("##0.0", 6))
+                                   ),
+                                   ...) {
+  # FIXME: This doesn't work - which requires that the input data be prepared
+  # using a similar approach
+  # x <- set_excel_style_class(
+  #   wb_data(wb, sheet = sheet),
+  #   cols = data,
+  #   sty = "currency"
+  # )
+
+  openxlsx2::wb_add_pivot_table(
+    wb = wb,
+    x = wb_data(wb, sheet = sheet),
+    # x = x,
+    dims = dims,
+    rows = rows,
+    data = data,
+    params = params,
+    fun = "SUM"
+  )
+}
