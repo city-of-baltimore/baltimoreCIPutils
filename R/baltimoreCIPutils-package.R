d35f95a8b0427a0cee629a640159480fc45da09e
Eli Pousson <eli.pousson@gmail.com>
2024-10-21 12:27:00 -0400

    Add package-level imports for openxlsx2

--- R/baltimoreCIPutils-package.R
+++ R/baltimoreCIPutils-package.R
@@ -4,7 +4,7 @@
 ## usethis namespace: start
 #' @import rlang
 #' @importFrom lifecycle deprecated
-#' @importFrom openxlsx2 wb_color wb_dims
+#' @importFrom openxlsx2 wb_color wb_dims wb_data current_sheet
 #' @importFrom tidyselect all_of any_of starts_with
 ## usethis namespace: end
 NULL
