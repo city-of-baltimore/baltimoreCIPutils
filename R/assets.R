cab95a69632fc2c835c3c932f152b766c03e7f4e
Eli Pousson <eli.pousson@gmail.com>
2024-10-21 21:39:14 -0400

    Fix issue w/ wd_proj_join_asset_id w/ non-default code cols

--- R/assets.R
+++ R/assets.R
@@ -1,18 +1,27 @@
 #' Join Asset ID values based on `wd_proj_asset_xwalk` reference data
 #'
-#' By default, [wd_proj_join_asset_id()] stores the asset ID values in a nested
-#' data frame list column to allow handling of projects with multiple matching
-#' assets.
+#' By default, [wd_proj_join_asset_id()] uses `multiple = "nested"` to store the
+#' asset ID values in a nested data frame list column to allow handling of
+#' projects with multiple matching assets.
 #'
-#' @keywords internal
+#' @param data A data frame with column name matching value in
+#'   `project_code_col`.
+#' @param project_code_col Project code column name in data.
+#' @param asset_id_col Asset ID column name.
+#' @param multiple Defaults to "nested" which nests the data by project code
+#'   column and then sets `multiple = "any"`. Passed to [dplyr::left_join()]
+#' @export
 wd_proj_join_asset_id <- function(data,
                                   project_code_col = "Project Code",
                                   asset_id_col = "asset_id",
                                   multiple = "nested") {
+  asset_xwalk_cols <- set_names(
+    c("Project Code", asset_id_col),
+    c(project_code_col, asset_id_col)
+  )
+
   asset_xwalk <- baltimoreCIPutils::wd_proj_asset_xwalk |>
-    dplyr::select(
-      all_of(c(project_code_col, asset_id_col))
-    )
+    dplyr::select(all_of(asset_xwalk_cols))
 
   if (multiple == "nested") {
     asset_xwalk <- asset_xwalk |>
