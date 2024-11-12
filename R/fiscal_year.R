270c8e74bb31e3ed69e192bc3f34886e4a2cb3c4
Eli Pousson <eli.pousson@gmail.com>
2024-11-12 12:20:14 -0500

    Improve handling of NA values
    Also rename year_abb type value to year_prefix_abb for consistency

--- R/fiscal_year.R
+++ R/fiscal_year.R
@@ -1,11 +1,36 @@
+#' Convert an object to a fiscal year string or date
+#'
+#' [fiscal_year()] uses [lubridate::as_date()] to return a fiscal year value
+#' based on any input vector coercible to dates.
+#'
+#' @param type One of "year", "year_prefix", "year_prefix_abb", "date_first", or
+#'   "date_last"
+#' @inheritDotParams lubridate::as_date
+#' @returns An integer (if type = "year"), character, or date (if type is
+#'   "date_first" or "date_last").
+#' @examples
+#' fiscal_year("2021-01-01")
+#'
+#' fiscal_year("2021-07-01")
+#'
+#' fiscal_year("2021-01-01", "year_prefix")
+#'
+#' fiscal_year("2021-01-01", "year_prefix_abb")
+#'
+#' fiscal_year("2021-01-01", "date_first")
+#'
+#' fiscal_year("2021-01-01", "date_ldate")
+#'
+#' @export
 fiscal_year <- function(x,
-                        type = c("year", "year_prefix", "year_abb",
+                        type = c("year", "year_prefix", "year_prefix_abb",
                                  "date_first", "date_last"),
-                        before = "FY") {
+                        before = "FY",
+                        ...) {
   type <- rlang::arg_match(type)
 
   if (!inherits(x, "Date")) {
-    x <- lubridate::as_date(x)
+    x <- lubridate::as_date(x, ...)
   }
 
   fy <- dplyr::if_else(
@@ -16,8 +41,8 @@ fiscal_year <- function(x,
 
   switch(type,
     year = fy,
-    year_prefix = paste0(before, fy),
-    year_abb = paste0(before, substr(fy, 3, 4)),
+    year_prefix = dplyr::if_else(!is.na(fy), paste0(before, fy), NA_character_),
+    year_prefix_abb = dplyr::if_else(!is.na(fy), paste0(before, substr(fy, 3, 4)), NA_character_),
     date_first = lubridate::as_date(paste0(fy - 1, "-07-01")),
     date_last = lubridate::as_date(paste0(fy, "-06-30"))
   )
