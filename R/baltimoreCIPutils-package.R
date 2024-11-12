36e86d05d9377e6cec0bf3a79f3da975b3c27780
Eli Pousson <eli.pousson@gmail.com>
2024-11-12 14:00:13 -0500

    Add missing package-level imports

--- R/baltimoreCIPutils-package.R
+++ R/baltimoreCIPutils-package.R
@@ -6,5 +6,7 @@
 #' @importFrom lifecycle deprecated
 #' @importFrom openxlsx2 wb_color wb_dims wb_data current_sheet
 #' @importFrom tidyselect all_of any_of starts_with
+#' @importFrom dplyr case_match case_when add_count bind_rows
+#' @importFrom stringr str_detect
 ## usethis namespace: end
 NULL
