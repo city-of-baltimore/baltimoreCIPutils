dd9c23af8b59f036e8efe57199988bb6cd9442db
Eli Pousson <eli.pousson@gmail.com>
2024-11-19 13:30:50 -0500

    Add gt formatting function

---
+++ R/gt.R
@@ -0,0 +1,25 @@
+#' Format fiscal year column values as currencies using `gt::fmt_currency()`
+#'
+#' [fmt_fy_span_currency()] is a wrapper for [gt::fmt_currency()] with a preset
+#' column selection using [curr_fy_span()].
+#'
+#' @inheritParams gt::fmt_currency
+#' @inheritDotParams gt::fmt_currency
+#' @keywords gt internal
+#' @export
+fmt_fy_span_currency <- function(data,
+                                 columns = tidyselect::all_of(curr_fy_span(year)),
+                                 ...,
+                                 year = getOption("baltimoreCIP.curr_year", 2026),
+                                 decimals = 0,
+                                 suffixing = "K") {
+  check_installed("gt")
+
+  gt::fmt_currency(
+    data = data,
+    columns = columns,
+    suffixing = suffixing,
+    decimals = decimals,
+    ...
+  )
+}
