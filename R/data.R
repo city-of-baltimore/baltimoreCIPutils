3d2b55c6a32f3b942714302928b7ac9f5bcf81be
Eli Pousson <eli.pousson@gmail.com>
2026-03-11 11:06:45 -0400

    Update wd_proj_asset_xwalk

--- R/data.R
+++ R/data.R
@@ -43,9 +43,9 @@
 #' Workday Project to Asset ID Crosswalk
 #'
 #' Reference data to join asset ID values to Workday Projects. Last updated
-#' 2026-02-23.
+#' 2026-03-11.
 #'
-#' @format A data frame with 922 rows and 7 variables:
+#' @format A data frame with 925 rows and 7 variables:
 #' \describe{
 #'   \item{`asset_id`}{Asset ID value, primarily DGS Building ID numbers}
 #'   \item{`Project Code`}{Join key for Project Code}
